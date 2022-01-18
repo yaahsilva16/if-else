a3f901c3323df8a01991a3eda9e0d0cd1689e712
Yasmin da Silva <95134741+yaahsilva16@users.noreply.github.com>
2022-01-18 13:25:49 -0300

    entregando meu if-else

---
+++ if-else/script.js
@@ -0,0 +1,38 @@
+let idade = 15
+
+if (idade >= 18) {
+    console.log("Parabéns, voce tem 18 anoooos!!")  
+}else{
+    console.log("Vish, fora. voce é menor de idade")
+}
+
+let humano =true
+
+if (idade >=18 && humano === true) {
+    console.log("Voce é um humano maior de idade")
+}else{
+    console.log("Voce sim é um humano, mas pro seu azar voce ainda é menor de idade")
+}
+
+let mes = "Dezembro"
+
+if (mes === "Dezembro"|| mes === "janeiro") {
+    console.log("Ei, voce faz aniversário em janeiro ou dezembro?")
+}
+
+let nome = "Yasmin"
+
+if(nome[0] == "R" || nome[0] == "R") {
+    console.log("Show de bola! seu nome começa com R")
+}else{
+    console.log("Poxa, seu nome não começa com R")
+}
+
+
+let lestName = "Boatinhos"
+
+if(lestName[0] == "S" || lestName.length == 6) {
+    console.log("Arrasou! seu nome tem todas as caracteristicas")
+}else{
+    console.log("Vish, seu sobrenome não tem todas as caracteristicas")
+}
